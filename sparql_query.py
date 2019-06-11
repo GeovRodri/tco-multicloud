9873bc52af9fd0e46d0b79f79fe5820f1be9b2e9
gilberto <gilberto.vampre@academico.ifg.edu.br>
2019-06-10 22:50:43 -0300

    adicionando ontologia

---
+++ sparql_query.py
@@ -0,0 +1,50 @@
+from owlready2 import *
+import json
+import pprint
+
+class SparqlQuery:
+    vms = ["alibaba", "aws", "azure", "google"]
+
+    def __init__(self):
+
+        onto_path.append("/home/gilberto//PycharmProjects/tco-multicloud/Ontology")
+
+        default_world.get_ontology("CloudIaaS3.owl").load()
+
+        sync_reasoner()  # reasoner is started and synchronized here
+        self.graph = default_world.as_rdflib_graph()
+
+    def search(self):
+
+        response = {}
+        iaasProvider = {}
+
+        response['generic_fields'] = ['cpu', 'hd', 'pricing', 'ram']
+
+        for provider in SparqlQuery.vms:
+
+            iaasProvider[provider] = []
+
+            query = """
+                            PREFIX b:<http://www.semanticweb.org/gilberto/ontologies/2019/4/CloudIaaS#>
+                            SELECT DISTINCT ?o
+                            WHERE  {{
+                            b:{} ?p ?s .
+                            ?s rdfs:comment ?o .
+                            }} """.format(provider)
+
+            resultsList = self.graph.query(query)
+
+            for item in resultsList:
+
+                o = str(item['o'].toPython())
+                o = re.sub(r'.*#', "", o)
+
+                iaasProvider[provider].append(o)
+
+        response['specific_fields'] = iaasProvider
+        return response
+
+runQuery = SparqlQuery()
+response = runQuery.search()
+pprint.pprint(response)  # just to show the output
