90e4c4d92346d3b4272e2e4a9ba719d609bbb6e4
geovany <geovanyscv@gmail.com>
2019-09-15 23:48:41 -0300

    Removendo campos para validacao

--- website/forms.py
+++ website/forms.py
@@ -10,37 +10,37 @@ FILTER_OPTIONS = (
 
 class PesquisaForm(forms.Form):
 
-    cpu_filter = forms.ChoiceField(
-        label='CPU filtro',
-        widget=forms.Select(attrs={'class': 'form-control'}),
-        choices=FILTER_OPTIONS)
+    # cpu_filter = forms.ChoiceField(
+    #     label='CPU filtro',
+    #     widget=forms.Select(attrs={'class': 'form-control'}),
+    #     choices=FILTER_OPTIONS)
 
     cpu = forms.IntegerField(
         label='CPU (opcional)',
         widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'CPU', 'type': 'number'}),
         required=False)
 
-    ram_filter = forms.ChoiceField(
-        label='RAM filtro',
-        widget=forms.Select(attrs={'class': 'form-control'}),
-        choices=FILTER_OPTIONS)
+    # ram_filter = forms.ChoiceField(
+    #     label='RAM filtro',
+    #     widget=forms.Select(attrs={'class': 'form-control'}),
+    #     choices=FILTER_OPTIONS)
 
     ram = forms.IntegerField(
         label='RAM (opcional)',
         widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'RAM', 'type': 'number'}),
         required=False)
 
-    hd_filter = forms.ChoiceField(
-        label='HD filtro',
-        widget=forms.Select(attrs={'class': 'form-control'}),
-        choices=FILTER_OPTIONS)
+    # hd_filter = forms.ChoiceField(
+    #     label='HD filtro',
+    #     widget=forms.Select(attrs={'class': 'form-control'}),
+    #     choices=FILTER_OPTIONS)
 
     hd = forms.IntegerField(
         label='HD (opcional)',
         widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'HD', 'type': 'number'}),
         required=False)
 
-    limit = forms.IntegerField(
-        label='Limite de Itens',
-        widget=forms.TextInput(attrs={'class': 'form-control'}),
-        initial=10)
+    # limit = forms.IntegerField(
+    #     label='Limite de Itens',
+    #     widget=forms.TextInput(attrs={'class': 'form-control'}),
+    #     initial=10)
